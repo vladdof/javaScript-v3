a4e211d3087802867fb915c638938bea7b6ad8e7
Vladislav Efanov <vladislav.efanov@gmail.com>
2018-08-20 15:13:35 +0400

    5 доп сделано

--- lesson 5 — dop/field-dop.js
+++ lesson 5 — dop/field-dop.js
@@ -1,6 +1,6 @@
 //1) Выведите на страницу текущую дату и время в формате '09:59:59 30.05.2018'
 let date = new Date(),
-	day = date.getDay(),
+	day = date.getDate(),
 	month = date.getMonth() + 1,
 	year = date.getFullYear(),
 //2) Напишите функцию, которая будет добавлять 0 перед днями и месяцами, которые состоят из одной цифры (из 1.6.2018 сделает 01.06.2018)
@@ -11,19 +11,54 @@ let date = new Date(),
 	seconds = date.getSeconds(),
 	fullTime = hours + ':' + minutes + ':' + seconds;
 
-console.log(fullDate + ' ' + fullTime);
+console.log(fullTime + ' ' + fullDate);
 
-console.log(date);
+// document.write(date.toLocaleString());
 
 //3) Напишите функцию, которая выводит на страницу текущий день недели на русском языке
-
-
+let	dayWeek = date.getDay() - 1,
+	week = [
+	"Понедельник", 
+	"Вторник", 
+	"Среда", 
+	"Четверг", 
+	"Пятница",
+	"Суббота",
+	"Воскресенье"
+];
+document.write("Текущий день недели — " + "<b>" + week[dayWeek] + "</b>" + '<br>');
+//еще вариант
+// document.write( date.toLocaleString('ru', {weekday: 'short'}) ); // 'сб'
 
 //4) Напишите функцию, которая выводит на страницу разницу между двумя датами в количестве дней
 let body = document.querySelector('body'),
 	inputDate1 = document.createElement("input"),
 	inputDate2 = document.createElement("input"),
-	inputOut = document.createElement('p');
+	inputOut = document.createElement('p'),
+	btn = document.createElement('button');
 
 	body.appendChild(inputDate1);
+	body.appendChild(inputDate2);
+	body.appendChild(inputOut);
+	body.appendChild(btn);
 
+	btn.textContent = "Рассчитать разницу";
+
+let date1,
+	date2;
+
+btn.addEventListener('click', function() {
+
+	date1 = inputDate1.value;
+	date2 = inputDate2.value;
+
+	let	date_1 = new Date(date1),
+		date_2 = new Date(date2),
+		minus = Math.floor(Math.abs(date_1 - date_2)/86400000);
+
+	console.log(date_2.toLocaleDateString());
+	console.log(date_1.toLocaleDateString());
+
+	inputOut.textContent = "Разность дат — " + minus + " дней";
+
+});
