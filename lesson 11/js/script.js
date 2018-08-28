8169a798f3abea690a6c18d410c5c0ae99414319
Vladislav Efanov <vladislav.efanov@gmail.com>
2018-08-29 00:22:49 +0400

    11 сделал

--- lesson 11/js/script.js
+++ lesson 11/js/script.js
@@ -147,18 +147,54 @@ window.addEventListener('DOMContentLoaded', function() {
 	message.success = 'Спасибо! Скоро мы с вами свяжемся';
 	message.failure = 'Что-т пошло не так...';
 
+	// получили всплывающую форму
 	let form = document.getElementsByClassName('main-form')[0],
 		input = form.getElementsByTagName('input'),
-		statusMessage = document.createElement('div');
 
+		// получили форму контакт
+		cont_form = document.getElementById('form'),
+		cont_input = cont_form.getElementsByTagName('input'),
+		// создали уведомление об отправке
+		statusMessage = document.createElement('div');
+		// добавили ему класс
 		statusMessage.classList.add('status');
+
+	cont_form.addEventListener('submit', function(event) {
+		event.preventDefault();
+		cont_form.appendChild(statusMessage);
+
+		let request = new XMLHttpRequest();
+
+		request.open("POST", 'server.php');
+		request.setRequestHeader("Content-Type", "application/x-www/form/urlencoded");
+
+		let formData = new FormData(cont_form);
+
+		request.send(formData);
+
+		request.onreadystatechange = () => {
+			if ( request.readyState < 4 ) {
+				statusMessage.innerHTML = message.loading;
+			}	else if ( request.readyState === 4 ) {
+				if ( request.status == 200 && request.status < 300 ) {
+					statusMessage.innerHTML = message.success;
+				} else {
+					statusMessage.innerHTML = message.failure;
+				}
+			}
+		}
+
+		for ( let i = 0; i < cont_input.length; i++ ) {
+			cont_input[i].value = '';
+		}
+	});
 
 	// событие на форму submit (отправка формы)
 	form.addEventListener('submit', function(event) {
 		// чтобы страница не перезагружалась
 		event.preventDefault();
 		// вставили сообщение
-		form.appendChild('statusMessage');
+		form.appendChild(statusMessage);
 
 		// ajax 
 		let request = new XMLHttpRequest();
