c54ee27d067228530362c60fe4bc346984590449
Vladislav Efanov <vladislav.efanov@gmail.com>
2018-08-28 12:25:09 +0400

    8 дополнительное сделал

--- lesson 7/notes №7/notes-lesson.js
+++ lesson 7/notes №7/notes-lesson.js
@@ -43,23 +43,101 @@
 
 
 //основы делегирования события с его родителя на его потомки
-let btns = document.getElementsByTagName('button'),
-	btn_block = document.querySelector('.btn-block');
+// let btns = document.getElementsByTagName('button'),
+// 	btn_block = document.querySelector('.btn-block');
 
-btn_block.addEventListener('click', function(event) {
-	//проверяем что правда есть и на тег button 
-	//большими буквами при проверке на тег
-	// if (event.target && event.target.tagName == 'BUTTON') {
-	// 	console.log("hello");
-	// }
+// btn_block.addEventListener('click', function(event) {
+// 	//проверяем что правда есть и на тег button 
+// 	//большими буквами при проверке на тег
+// 	// if (event.target && event.target.tagName == 'BUTTON') {
+// 	// 	console.log("hello");
+// 	// }
 
-	//также на классы проверить можно
-	// if (event.target && event.target.className == 'first') {
-	// 	console.log("hello");
-	// }
+// 	//также на классы проверить можно
+// 	// if (event.target && event.target.className == 'first') {
+// 	// 	console.log("hello");
+// 	// }
 
-	//также другим методом можно проверить по классу. ищем кнопку с классом first 
-	if (event.target && event.target.matches('button.first')) {
-		console.log("hello");
-	}	
-});
+// 	//также другим методом можно проверить по классу. ищем кнопку с классом first 
+// 	if (event.target && event.target.matches('button.first')) {
+// 		console.log("hello");
+// 	}	
+// });
+
+// let block = document.querySelector(".box");
+
+// 	//рисовать анимацию draw, duration - продолжительность
+// 	function animate(draw, duration) {
+// 		//время с начала загрузки страницы
+// 		let start = performance.now();
+// 		// рекурсивный вызов анимации
+// 		requestAnimationFrame( function animate(time) { 
+// 		//time это start при вызове анимации
+
+// 			// сколько вр прошло с начала анимации
+// 			let timePassed = time - start;
+
+// 			// если вр выполнения анимации привысело > чем заданная длительность, то фикс.конец анимации
+// 			if (timePassed > duration) {
+// 				timePassed = duration;
+// 			}
+// 			// рисуем соотношение анимации в момент timePassed
+// 			draw(timePassed);
+
+// 			// если время анимации не закончено, то продолжаем анимацию
+// 			if(timePassed < duration) {
+// 				requestAnimationFrame(animate);
+// 			}
+// 		});
+// 	}
+
+// 	block.addEventListener('click', function() {
+// 		animate(
+// 			function(timePassed) {
+// 				block.style.left = timePassed / 5 + 'px';
+// 			},
+// 			2000);
+// 	});
+
+let block = document.querySelector(".box");
+
+	//рисовать анимацию draw, duration - продолжительность
+	function animate({timing, draw, duration}) {
+		//время с начала загрузки страницы
+		let start = performance.now();
+		// рекурсивный вызов анимации
+		requestAnimationFrame( function animate(time) { 
+		//time это start при вызове анимации
+
+			// сколько вр прошло с начала анимации
+			// от 0 до 1
+			let timePassed = (time - start) / duration;
+
+			// если вр выполнения анимации привысело > чем заданная длительность, то фикс.конец анимации
+			if (timePassed > 1) {
+				timePassed = 1;
+			}
+
+			// рассчитываем текущее время нашей анимации
+			let progress = timing(timePassed);
+			// рисуем соотношение анимации в зависимости от текущ состояния
+			draw(progress);
+
+			// если время анимации не закончено, то продолжаем анимацию
+			if(timePassed < 1) {
+				requestAnimationFrame(animate);
+			}
+		});
+	}
+
+	block.addEventListener('click', function() {
+		animate( {
+			duration: 2000,
+			timing: function back(x, timePassed) {
+				return Math.pow(timePassed, 2) * ((x + 1) * timePassed - x);
+			}.bind(null, 1.5),
+			draw: function(progress) {
+				block.style.left = progress * 500 + 'px';
+ 			}
+		});
+	});
---
+++ lesson 8/js/script — dop.js
@@ -0,0 +1,28 @@
+//ждет пока структура html/css загрузяться, легче чем load
+window.addEventListener('DOMContentLoaded', function() {
+
+	// плавная анимация
+	let link = document.querySelectorAll('[href^="#"]');
+
+	for (let i = 0; i < link.length; i++) {
+		link[i].addEventListener('click' , function(event) {
+			event.preventDefault();
+			// где был клик
+			let target = event.target.getAttribute('href').slice(1),
+				link = document.getElementById(target),
+				start = Date.now();
+
+			id = setInterval( function() {
+				let t = Date.now() - start;
+
+				if (t > 2000) {
+					clearInterval(id);
+					return;
+				} else {
+					window.scrollBy(0,link.getBoundingClientRect().top / 20 - 2); 
+				}
+			}, 20);
+		});
+	};
+
+});
